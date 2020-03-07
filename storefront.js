495ec4c056e87859b6c2cc4b206b78b9516b7648
Mlipphardt <matthewlipphardt@gmail.com>
2020-03-07 16:58:31 -0500

    User may now purchase an item, with table updated properly.

--- storefront.js
+++ storefront.js
@@ -23,6 +23,7 @@ function storeWelcome(){
     }).then(function(answer){
         switch (answer.department){
             case "grocery":
+            case "pharmacy":
                 chooseAction(answer.department);
                 break;
         }
@@ -38,16 +39,72 @@ function chooseAction(department){
         name: "action",
         type: "list",
         message: "What would you like to do?",
-        choices: ["Browse inventory"]
+        choices: ["Browse inventory", "Purchase an item"]
     }).then(function(answer){
         switch (answer.action){
             case "Browse inventory":
                 readInventory(department);
+                break;
+            case "Purchase an item":
+                purchaseItem(department);
                 break;
         }
     })
 }
 
 function readInventory(department){
-    console.log("We're going to the " + department)
+    var query = "SELECT * FROM " + department;
+      connection.query(query, function(err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(
+              "ID: " +
+                res[i].id +
+                " || Item: " +
+                res[i].item +
+                " || Price: " +
+                res[i].price +
+                " || Description: " +
+                res[i].description +
+                " || Stock " +
+                res[i].stock
+            );
+          }
+        chooseAction(department);
+    });
+}
+
+function purchaseItem(department){
+    inquirer.prompt([{
+        name: "id",
+        type: "input",
+        message: "Select an item by its ID to purchase."
+    }, {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like today?"
+    }]).then(function(answer){
+        let itemInfo = "SELECT price FROM " + department + " WHERE ?"
+        let price;
+        connection.query(itemInfo, { id: answer.id }, function(err, res){
+            if (err) throw err;
+            console.log(res)
+            price = res[0].price.toString();
+            console.log(price);
+            console.log(price.length);
+            if(price.length == 3){
+                price = price + "0";
+                console.log(price);
+            }
+            chooseAction(department);
+        });
+        let query = "UPDATE " + department +  " SET stock=stock-1 WHERE ?"
+        connection.query(query, {id: answer.id}, function(err, res){
+            if (err) throw err;
+            console.log(res)
+            console.log("Payment received in the amount of $" + price + "!")
+            console.log("Thank you for your purchase!")
+        });
+    });
+    
 }
